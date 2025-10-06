195164c6a14c561df0f0bc88646fc45bf91ffdc7
Ferreira <pedroferreira.gomes03@gmail.com>
2025-10-06 20:19:34 -0300

    feat: fanilizando base do projeto

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -2,8 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from './routes/Login/index.tsx'
+import Cadastro from './routes/Cadastro/index.tsx'
+import Error from './routes/Error/index.tsx'
+
+const router = createBrowserRouter([
+  {path:"/", element:<App/>, errorElement: <Error/> , children:[
+    {
+      path: "/",
+      element: <Login/>
+    },
+    {
+      path: "/cadastro",
+      element: <Cadastro/>
+    }
+  ]}
+]);
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
