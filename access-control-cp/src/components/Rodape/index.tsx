fccf0da10d3e03f5772fd2c168935967b460e9db
GabrielSilva <gabrielbebesilva247@gmail.com>
2025-10-06 23:19:04 -0300

    issue: corrigi rodape deixando fixo na pagina.

--- access-control-cp/src/components/Rodape/index.tsx
+++ access-control-cp/src/components/Rodape/index.tsx
@@ -2,7 +2,7 @@ export default function Rodape() {
   const anoAtual = new Date().getFullYear();
 
   return (
-    <footer className="w-full bg-gray-800 text-white text-center p-4 fixed bottom-0">
+    <footer className="w-full bg-gray-800 text-white text-center p-4">
       <p>&copy; {anoAtual} Access Control. Todos os direitos reservados.</p>
     </footer>
   );
